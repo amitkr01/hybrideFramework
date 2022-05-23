ea772cbafbe573bf8371b15768f8315e54b13dc4
mindsdet153 <mindsdet153@mindc1d457.makemylabs.in>
2022-05-23 13:47:29 +0530

    update commit

--- hybrideFrameworkProject/src/main/java/com/runner/LoginPageTest.java
+++ hybrideFrameworkProject/src/main/java/com/runner/LoginPageTest.java
@@ -8,29 +8,70 @@ import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+
 import com.pageObject.LoginPage;
 import com.resource.Base;
+import com.utility.Excelutil;
+
 
 public class LoginPageTest extends Base {
-	public static Logger log = LogManager.getLogger(Base.class.getName());
-	@Test
+	
+	@BeforeTest
 	public void loginpage() throws IOException
 	{
 		driver = initializeDriver();
 		
-		driver.get(prop.getProperty("url"));
 		
+	}
+			
+	@Test(dataProvider="test1data")
+	public void test1(String username, String password) {
+		System.out.println(username + " | "+ password);
+		driver.get(prop.getProperty("url"));
 		LoginPage l = new LoginPage(driver);
 		
 		l.LoginIcon().click();
-		log.info("open login page");
 		l.Loginbtn().click();
-		l.Email().sendKeys("amit123@gmail.com");
-		log.info("enter eamil address");
-		l.Password().sendKeys("amit1234");
-		log.info("Enter the password");
+		l.Email().sendKeys(username);
+		l.Password().sendKeys(password);
 		l.Login().click();
-		log.info("click submit button");
+				
+	}
+	
+	
+	@DataProvider(name = "test1data")
+	public Object[][] getData() {
+		String excelPath="C:\\Users\\mindsdet153\\git\\hybrideFramework\\hybrideFrameworkProject\\Excel\\getdata.xlsx";
+		Object data[][] = testdata(excelPath, "sheet1");
+		return data;
+	}
+	
+	
+	
+	public Object[][] testdata(String excelPath, String sheetName) {
+		
+		Excelutil excel = new Excelutil(excelPath, sheetName);
+		
+		int rowcount = excel.getRowCount();
+		int colcount = excel.getColCount();
+		
+		Object data[][] = new Object[rowcount-1][colcount];
+		
+		for(int i=1; i<rowcount; i++) {
+			for(int j=0; j<colcount; j++) {
+				
+				String cellData = excel.getCellDataString(i, j);
+				//System.out.print(cellData + " | " );
+				data[i-1][j]  = cellData;
+										
+			}
+			//System.out.println();
+			
+		}
+		return data;
+		
 	}
 	
 	@AfterTest
--- hybrideFrameworkProject/src/main/java/com/utility/Excelutil.java
+++ hybrideFrameworkProject/src/main/java/com/utility/Excelutil.java
@@ -4,7 +4,7 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Excelutil {
-
+	public static String projectPath;
 	public static XSSFWorkbook workbook;
 	public static XSSFSheet sheet;
 
@@ -21,43 +21,55 @@ public class Excelutil {
 
 	
 
-	public static void getRowCount()
+	public static int getRowCount()
 	{
+		int rowCount = 0;
 
 		try {
+				rowCount = sheet.getPhysicalNumberOfRows();
+			//System.out.println(rowCount);
+
+		} catch (Exception e) {
+						e.printStackTrace();
+		}
+		return rowCount;
+	}
+	public static int getColCount()
+	{
+		int colCount = 0;
+
+		try {
+			    colCount = sheet.getRow(0).getPhysicalNumberOfCells();
+			//System.out.println(colCount);
+
+		} catch (Exception e) {
+					e.printStackTrace();
+		}
+		return colCount ;
+	}
+
+
+	public static String getCellDataString(int rowNum, int colNum) {
+		String cellData = null;
+		try {
+			cellData= sheet.getRow(rowNum).getCell(colNum).getStringCellValue();
 		
-			int rowCount = sheet.getPhysicalNumberOfRows();
-			System.out.println(rowCount);
 
 		} catch (Exception e) {
-				e.printStackTrace();
+					e.printStackTrace();
 		}
+		return cellData;
 	}
-
-
-	public static void getCellDataString(int rowNum, int colNum) {
+	
+	public static double getCellDataNumber(int rowNum, int colNum) {
+		double cellData =0;
 		try {
-
-
-			String cellCount= sheet.getRow(rowNum).getCell(colNum).getStringCellValue();
-			System.out.println(cellCount);
-
-
+			cellData = sheet.getRow(rowNum).getCell(colNum).getNumericCellValue();
+			//System.out.println(cellData);
 
 		} catch (Exception e) {
-				e.printStackTrace();
+					e.printStackTrace();
 		}
-
-	}
-	public static void getCellDataNumber(int rowNum, int colNum) {
-		try {
-
-			double cellData = sheet.getRow(rowNum).getCell(colNum).getNumericCellValue();
-			System.out.println(cellData);
-
-		} catch (Exception e) {
-				e.printStackTrace();
-		}
-
+		return cellData;
 	}
 }
